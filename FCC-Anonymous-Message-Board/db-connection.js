c8ca71ac4072b0ca3d7a566cbe7d85d794fca4e7
trissentety2 <152235182+trissentety2@users.noreply.github.com>
2023-12-02 04:19:59 -0800

    Delete FCC-Anonymous-Message-Board directory

--- FCC-Anonymous-Message-Board/db-connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const db = mongoose.connect(process.env.DB)
-    // .then(() => console.log("db is connected"))
-    // .catch((err) => console.log(err));
-
-module.exports = db;
